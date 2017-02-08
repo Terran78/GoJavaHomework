1911c6daedd9a19bc7fb156d6d8383e47d1a7093
Terran78 <stromilov@mail.ru>
2017-02-08 22:59:31 +0200

    New project

---
+++ src/main/java/com/stromilov/practice/modul01/Fifth.java
@@ -0,0 +1,7 @@
+package com.stromilov.practice.modul01;
+
+public class Fifth {
+    public static void main(String[] args) {
+        System.out.println("My first class on Java");
+    }
+}
---
+++ src/main/java/com/stromilov/practice/modul01/SecondClass.java
@@ -0,0 +1,7 @@
+package com.stromilov.practice.modul01;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("My first class on Java");
+    }
+}
