6a8e1060474357252d0f7ee20d96f1010f286a95
Terran78 <stromilov@mail.ru>
2017-02-09 23:48:35 +0200

    Correct HomeWork

--- src/main/java/com/stromilov/practice/module02/HomeWork_2_1/HomeWork_2_1.java
+++ src/main/java/com/stromilov/practice/module02/HomeWork_2_1/HomeWork_2_1.java
@@ -3,72 +3,216 @@ package com.stromilov.practice.module02.HomeWork_2_1;
 public class HomeWork_2_1 {
 
     public static void main(String[] args) {
-        double[] doubleArray = {-5.2, -1.5, 2.4, 2.5, 3.7, 3.5, 4.4, -4.5, 4.7, 4.9};
-        int[] intArray = {43, 2, 3, 4, 5, 6, 7, 8, -9, 10};
 
-        System.out.println("Sum double -> " + Sum(doubleArray));
-        System.out.println("Sum int -> " + Sum(intArray));
-
-        System.out.println("Min double -> " + doMin);
-        System.out.println("Min int -> " + intMin);
-
-        System.out.println("Max double -> " + doMax);
-        System.out.println("Max int -> " + intMax);
-
-        System.out.println("Max positive double -> " + doMaxPos);
-        System.out.println("Max positive int -> " + intMaxPos);
-
-        System.out.println("Multiple double -> " + doMult);
-        System.out.println("Multiple int -> " + intMult);
-
-        System.out.println("Modulus 1 and last double -> " + (doubleArray[0]%doubleArray[doubleArray.length -1]));
-        System.out.println("Modulus 1 and last ins -> " + (intArray[0]%intArray[intArray.length -1]));
-
-        System.out.println("Second largest double -> " + doSecLar);
-        System.out.println("Second largest int -> " + intSecLar);
+        int[] intArray = {-14, 21, 32, 4, 54, 61, -7, 88, 9, -12};
+        double[] doubleArray = {-14.2, 21.5, 32.1, 4.2, 54.9, 61.6, -7.7, 88.5, 9.1, -12.5};
 
 
-        // Sum all elements of int or double
-        public void  Sum(double[] array){
+        System.out.println("Total is " + sum(intArray));
+        System.out.println("Total is " + sum(doubleArray));
 
-            int Sum = 0;
-            for (int i = 0; i < array.length; i++) {
-            Sum += array[i];
-        }
-        return Sum;
-    }
+        System.out.println("Maximum number is " + max(intArray));
+        System.out.println("Maximum number is " + max(doubleArray));
 
+        System.out.println("Minimal number is " + min(intArray));
+        System.out.println("Minimal number is " + min(doubleArray));
 
-        double doSum, doMin, doMax, doMaxPos, doMult, doSecLar;
-        doSum =  doMaxPos = doSecLar = 0;
-        doMult = 1;
-        doMin = doMax = doubleArray[0];
-        int intSum, intMin, intMax, intMaxPos, intMult, intSecLar;
-        intSum = intMaxPos = intSecLar = 0;
-        intMult = 1;
-        intMin = intMax = intArray[0];
+        System.out.println("Maximum positive number is " + maxPositive(intArray));
+        System.out.println("Maximum positive number is " + maxPositive(doubleArray));
 
-        for (int i = 0; i < doubleArray.length; i++) {
-            doSum += doubleArray[i];  // считаем суммы
-            if (doMin > doubleArray[i]) doMin = doubleArray[i];
-            if (doMax < doubleArray[i]) doMax = doubleArray[i];
-            if (doMaxPos < doubleArray[i] && doubleArray[i] > 0) doMaxPos = doubleArray[i];
-            doMult *= doubleArray[i]; // multiplication
+        System.out.println("Second maximum number after " + max(intArray) + " is " + secondLargest(intArray));
+        System.out.println("Second maximum number after " + max(doubleArray) + " is " + secondLargest(doubleArray));
 
-            intSum += intArray[i]; // т.к размеры массивов одинаковы, то чтоб не повторяться.
-            if (intMin > intArray[i]) intMin = intArray[i];
-            if (intMax < intArray[i]) intMax = intArray[i];
-            if (intMaxPos < intArray[i] && intArray[i] > 0) intMaxPos = intArray[i];
-            intMult *= intArray[i];
-        }
-        for (int i = 0; i < 10 ; i++) {
-            if (doSecLar < doubleArray[i] && doubleArray[i] != doMax) doSecLar = doubleArray[i];
-            if (intSecLar < intArray[i] && intArray[i] != intMax) intSecLar = intArray[i];
+        double multi = multiplication(doubleArray);
 
-        }
+        double mod = modulus(doubleArray);
 
+        System.out.println("The result of multiplying of array values is " + multiplication(intArray));
+        System.out.println("The result of multiplying of array values is " + multi);
 
+        System.out.println("The result of modulus operation is " + modulus(intArray));
+        System.out.println("The result of modulus operation is " + mod);
 
 
     }
+
+    //1 - Просчитываем сумму элементов массива типа int
+
+    public static int sum(int[] array) {
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return sum;
+    }
+
+    //1 - Просчитываем сумму элементов массива типа double
+
+    public static double sum(double[] arrayD) {
+
+        double sum = 0;
+        for (int i = 0; i < arrayD.length; i++) {
+            sum = sum + arrayD[i];
+        }
+        return sum;
+    }
+
+    //2 - Находим максимальное значение числа в массиве типа int
+
+    public static int max(int[] array) {
+
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i])
+                max = array[i];
+        }
+        return max;
+    }
+
+    //2 - Находим максимальное значение числа в массиве типа double
+
+    public static double max(double[] arrayD) {
+
+        double max = arrayD[0];
+        for (int i = 0; i < arrayD.length; i++) {
+            if (max < arrayD[i])
+                max = arrayD[i];
+        }
+        return max;
+    }
+
+    //3 - Находим минимальное значение числа в массиве типа int
+
+    public static int min(int[] array) {
+
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i])
+                min = array[i];
+        }
+        return min;
+    }
+
+    //3 - Находим минимальное значение числа в массиве типа double
+
+    public static double min(double[] arrayD) {
+
+        double min = arrayD[0];
+        for (int i = 0; i < arrayD.length; i++) {
+            if (min > arrayD[i])
+                min = arrayD[i];
+        }
+        return min;
+    }
+
+    //4 - Находим максимальное положительное значение числа в массиве типа int
+
+    public static int maxPositive(int[] array) {
+
+        int maxPositive = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (maxPositive < array[i] && array[i] > 0)
+                maxPositive = array[i];
+        }
+        return maxPositive;
+    }
+
+    //4 - Находим максимальное положительное значение числа в массиве типа double
+
+    public static double maxPositive(double[] arrayD) {
+
+        double maxPositive = arrayD[0];
+        for (int i = 0; i < arrayD.length; i++) {
+            if (maxPositive < arrayD[i] && arrayD[i] > 0)
+                maxPositive = arrayD[i];
+        }
+        return maxPositive;
+    }
+
+    //5 - Находим результат умножения всех чисел в массиве типа int
+
+    public static long multiplication(int[] array) {
+
+        long multiplication = 1;
+        for (int i = 0; i < array.length; i++) {
+            multiplication *= array[i];
+        }
+        return multiplication;
+    }
+
+    //5 - Находим результат умножения всех чисел в массиве типа double
+
+    public static double multiplication(double[] arrayD) {
+
+        double multiplication = 1;
+        for (int i = 0; i < arrayD.length; i++) {
+            multiplication *= arrayD[i];
+        }
+        return multiplication;
+    }
+
+    //6 - Находим результат деления по модулю первого и последнего чисел в массиве типа int
+
+    public static long modulus(int[] array) {
+
+        long modulus = 1;
+        long firstNumber = array[0];
+        long lastNumber = array[array.length - 1];
+
+        if (lastNumber != 0) {
+            modulus = firstNumber % lastNumber;
+        } else modulus = 0;
+        return modulus;
+    }
+
+    //6 - Находим результат деления по модулю первого и последнего чисел в массиве типа double
+
+    public static double modulus(double[] arrayD) {
+
+        double modulus = 1;
+        double firstNumber = arrayD[0];
+        double lastNumber = arrayD[arrayD.length - 1];
+
+        if (lastNumber != 0) {
+            modulus = firstNumber % lastNumber;
+        } else modulus = 0;
+
+        return modulus;
+    }
+
+    //7 - Находим второе наибольшее число в массиве типа int (после того, как первое наибольшее уже найдено)
+
+    public static int secondLargest(int[] array) {
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int num : array) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest) {
+                secondLargest = num;
+            }
+        }
+        return secondLargest;
+    }
+
+    //7 - Находим второе наибольшее число в массиве типа double (после того, как первое наибольшее уже найдено)
+
+    public static double secondLargest(double[] arrayD) {
+
+        double largest = Integer.MIN_VALUE;
+        double secondLargest = Integer.MIN_VALUE;
+        for (double num : arrayD) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest) {
+                secondLargest = num;
+            }
+        }
+        return secondLargest;
+    }
+
 }
