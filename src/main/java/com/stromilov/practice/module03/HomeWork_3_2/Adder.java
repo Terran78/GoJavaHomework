281d27f2aaee1e400bb7d38e614b8e8f92ec1c3a
Terran78 <stromilov@mail.ru>
2017-02-11 01:07:11 +0200

    HomeWork_3_2

---
+++ src/main/java/com/stromilov/practice/module03/HomeWork_3_2/Adder.java
@@ -0,0 +1,12 @@
+package com.stromilov.practice.module03.HomeWork_3_2;
+
+public class Adder extends Arithmetic {
+
+    boolean check(Integer a, Integer b) {
+
+        if (a>=b) {
+            return true;
+        } else
+            return false;
+    }
+}
---
+++ src/main/java/com/stromilov/practice/module03/HomeWork_3_2/Arithmetic.java
@@ -0,0 +1,11 @@
+package com.stromilov.practice.module03.HomeWork_3_2;
+
+public class Arithmetic {
+
+    Integer a;
+    Integer b;
+
+    int add (Integer a, Integer b) {
+        return a + b;
+    }
+}
